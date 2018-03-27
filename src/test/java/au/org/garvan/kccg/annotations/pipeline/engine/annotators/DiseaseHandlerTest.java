21eae52dddf538d441ea192fa9280eead0699b1d
Ahmed Muaz <a.muaz@garvan.org.au>
2018-03-27 14:19:31 +1100

    [DiseaseAnnotations] disabled test

--- src/test/java/au/org/garvan/kccg/annotations/pipeline/engine/annotators/DiseaseHandlerTest.java
+++ src/test/java/au/org/garvan/kccg/annotations/pipeline/engine/annotators/DiseaseHandlerTest.java
@@ -10,18 +10,18 @@ import static org.junit.Assert.*;
 
 public class DiseaseHandlerTest {
 
-    DiseaseHandler diseaseHandler = new DiseaseHandler("mondo.json");
-    @Test
-    public void readFile() {
-            diseaseHandler.readFile();
-
-    }
-
-
-    @Test
-    public void processAndUpdateDocument() {
-        APDocument testDoc = new APDocument("Angiosarcoma is a rare vascular soft tissue tumor of endothelial origin most commonly seen in the elderly as a primary cutaneous head and neck malignancy. Furthermore, a peritoneal angiosarcoma is an exceedingly rare entity. This is the second case of primary peritoneal angiosarcoma reported in literature that is not associated with prior radiotherapy. Herein, we describe a case of primary peritoneal angiosarcoma metastatic to both the liver and bone in a male patient with metachronous renal cell carcinoma and parathyroid adenoma.");
-        testDoc.getProcessingProfile().getAnnotationRequests().add(AnnotationType.DISEASE);
-        testDoc.hatch(1);
-    }
+//    DiseaseHandler diseaseHandler = new DiseaseHandler("mondo.json");
+//    @Test
+//    public void readFile() {
+//            diseaseHandler.readFile();
+//
+//    }
+//
+//
+//    @Test
+//    public void processAndUpdateDocument() {
+//        APDocument testDoc = new APDocument("Angiosarcoma is a rare vascular soft tissue tumor of endothelial origin most commonly seen in the elderly as a primary cutaneous head and neck malignancy. Furthermore, a peritoneal angiosarcoma is an exceedingly rare entity. This is the second case of primary peritoneal angiosarcoma reported in literature that is not associated with prior radiotherapy. Herein, we describe a case of primary peritoneal angiosarcoma metastatic to both the liver and bone in a male patient with metachronous renal cell carcinoma and parathyroid adenoma.");
+//        testDoc.getProcessingProfile().getAnnotationRequests().add(AnnotationType.DISEASE);
+//        testDoc.hatch(1);
+//    }
 }
