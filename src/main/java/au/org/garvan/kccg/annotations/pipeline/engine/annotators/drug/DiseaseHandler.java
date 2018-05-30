5425823b4900e74d57b09d0cd16b5779a345f620
Ahmed Muaz <a.muaz@garvan.org.au>
2018-05-30 20:29:39 +1000

    [DrugAnnotations] Initial partial setup

---
+++ src/main/java/au/org/garvan/kccg/annotations/pipeline/engine/annotators/drug/DiseaseHandler.java
@@ -0,0 +1,277 @@
+package au.org.garvan.kccg.annotations.pipeline.engine.annotators.drug;
+
+import au.org.garvan.kccg.annotations.pipeline.engine.connectors.AffinityConnector;
+import au.org.garvan.kccg.annotations.pipeline.engine.entities.lexical.Annotation;
+import au.org.garvan.kccg.annotations.pipeline.engine.entities.lexical.Disease.APDisease;
+import au.org.garvan.kccg.annotations.pipeline.engine.entities.lexical.mappers.APMultiWordAnnotationMapper;
+import au.org.garvan.kccg.annotations.pipeline.engine.entities.lexical.mappers.AnnotationHit;
+import au.org.garvan.kccg.annotations.pipeline.engine.entities.lexical.mappers.AnnotationTerm;
+import au.org.garvan.kccg.annotations.pipeline.engine.entities.lexical.mappers.AnnotationTermComparitor;
+import au.org.garvan.kccg.annotations.pipeline.engine.entities.linguistic.APDocument;
+import au.org.garvan.kccg.annotations.pipeline.engine.entities.linguistic.APSentence;
+import au.org.garvan.kccg.annotations.pipeline.engine.entities.linguistic.APToken;
+import au.org.garvan.kccg.annotations.pipeline.engine.enums.AnnotationType;
+import au.org.garvan.kccg.annotations.pipeline.engine.utilities.Common;
+import au.org.garvan.kccg.annotations.pipeline.engine.utilities.Pair;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+@AllArgsConstructor
+@Component
+public class DiseaseHandler{
+    private final Logger slf4jLogger = LoggerFactory.getLogger(DiseaseHandler.class);
+
+    @Autowired
+    private static AffinityConnector affinityServiceConnector;
+
+
+    private Map<String, APDisease> mondoDiseases;
+    private Map<String, String> diseaseLabelToMondo;
+
+    private static String STANDARD;
+    private static String VERSION;
+
+    /***
+     * Init for index reading
+     */
+    @Autowired
+    public DiseaseHandler(){
+        mondoDiseases = new HashMap<>();
+        diseaseLabelToMondo = new HashMap<>();
+        readFile("mondoAnnotationIndex.json");
+        affinityServiceConnector = new AffinityConnector();
+
+    }
+
+
+    /***
+     * Annotation function called from document preprocessor.
+     * @param apDocument
+     * @param articleId
+     */
+
+    //TODO: Split in two functions, one for calling affinity and other to fetch results.
+    public void processAndUpdateDocument(APDocument apDocument, int articleId) {
+
+        //TODO: Call
+        List<AnnotationHit> diseaseHits =   affinityServiceConnector.annotateAbstract(apDocument.getCleanedText(), articleId, "en");
+
+
+        //TODO: Add call and fetch result
+        for(AnnotationHit dHit: diseaseHits){
+            APDisease selectedDisease = getDisease(dHit.getAnnotationID());
+            List<List<AnnotationTerm>> chainedTerms = chainAnnotationTerms(dHit.getHits());
+            for(List<AnnotationTerm> term : chainedTerms){
+                Pair<Integer,Integer> offsetRange = getOffsetRange(term);
+                Pair<APSentence, List<APToken>> documentMap = apDocument.getSentenceAndTokenWithTokenOffsets(offsetRange.getFirst(), offsetRange.getSecond());
+
+                if(documentMap != null){
+                    boolean validToken = true;
+                    if(documentMap.getSecond().size()==1 ){
+                        validToken= Common.STOPPING_POS.contains(documentMap.getSecond().get(0).getPartOfSpeech()) ? false : true;
+                    }
+
+                    if(validToken) {
+                        Annotation annotation = new Annotation();
+                        annotation.setStandard(STANDARD);
+                        annotation.setVersion(VERSION);
+                        annotation.setType(AnnotationType.DISEASE);
+                        annotation.setEntity(selectedDisease);
+                        annotation.setTokenIDs(documentMap.getSecond().stream().map(t -> t.getId()).collect(Collectors.toList()));
+                        annotation.setTokenOffsets(documentMap.getSecond().stream().map(t -> t.getSentOffset()).collect(Collectors.toList()));
+                        annotation.setNegated(false);
+                        documentMap.getFirst().getAnnotations().add(annotation);
+                    }
+                }
+
+
+            }
+
+        }
+
+        //POINT: Longest Match
+        sustainLongestMatch(apDocument);
+
+    }
+
+    private void sustainLongestMatch(APDocument apDocument){
+        List<APSentence> apSentences = apDocument.getSentences().stream().filter(s->s.getAnnotations().stream().filter(a->a.getType().equals(AnnotationType.DISEASE)).collect(Collectors.toList()).size()>1).collect(Collectors.toList());
+        for (APSentence candidateSentence : apSentences){
+
+            List<Annotation> annotations = candidateSentence.getAnnotations().stream().filter(a->a.getType().equals(AnnotationType.DISEASE)).collect(Collectors.toList());
+            List<Integer> removals = new ArrayList<>();
+            for(Integer x = 0; x< annotations.size(); x++) {
+                for(Integer y = x+1; y<annotations.size(); y++)
+                {
+                    if(annotations.get(x).getTokenIDs().size() != annotations.get(y).getTokenIDs().size()) {
+                        Integer largerAnnotationIndex = annotations.get(x).getTokenIDs().size() > annotations.get(y).getTokenIDs().size()? x :y;
+                        Integer smallAnnotationIndex =  annotations.get(x).getTokenIDs().size() > annotations.get(y).getTokenIDs().size()? y :x;
+
+                        if(annotations.get(largerAnnotationIndex).getTokenIDs().containsAll(annotations.get(smallAnnotationIndex).getTokenIDs())){
+                            slf4jLogger.debug(String.format("Dissolving smaller annotation. Document id:%d |  LargeTokens:%s  | SmallTokens:%s ", apDocument.getId(), annotations.get(largerAnnotationIndex).getTokenIDs().toString(), annotations.get(smallAnnotationIndex).getTokenIDs().toString()));
+                            candidateSentence.getAnnotations().remove(annotations.get(smallAnnotationIndex));
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+
+        }
+
+
+    }
+
+    public Pair<Integer, Integer> getOffsetRange(List<AnnotationTerm> annotationTerms){
+        Integer begin = 100000;
+        Integer end = -1;
+        for(AnnotationTerm annotationTerm: annotationTerms){
+            if(annotationTerm.getStartOffset()<=begin)
+                begin= annotationTerm.getStartOffset();
+            if(annotationTerm.getEndOffset()>=end)
+                end = annotationTerm.getEndOffset();
+        }
+        return new Pair<>(begin, end);
+    }
+
+    public static List<List<AnnotationTerm>> chainAnnotationTerms(List<AnnotationTerm> terms){
+        List<List<AnnotationTerm>> chains = new ArrayList<>();
+        terms.sort(new AnnotationTermComparitor());
+        AnnotationTerm first = terms.get(0);
+        List<AnnotationTerm> smallChain = new ArrayList<>();
+        smallChain.add(first);
+        if(terms.size()>1)
+        {
+            for(int x = 1; x<terms.size(); x++){
+                AnnotationTerm second = terms.get(x);
+                if(second.getTokenStartIndex()-1 == first.getTokenStartIndex()){
+                    smallChain.add(second);
+                }
+                else
+                {
+                    chains.add(smallChain);
+                    smallChain = new ArrayList<>();
+                    smallChain.add(second);
+                }
+                first = second;
+
+                if(x==terms.size()-1){
+                    chains.add(smallChain);
+                }
+            }
+
+        }
+        else
+        {
+            chains.add(smallChain);
+        }
+        return chains;
+    }
+
+
+    /***
+     * Helping function for autocomplete.
+     * @param infix
+     * @return
+     */
+    public List<APMultiWordAnnotationMapper> searchDisease(String infix){
+        return diseaseLabelToMondo.entrySet().stream().filter(x->x.getKey().contains(infix.toLowerCase())).map(p-> new APMultiWordAnnotationMapper(p.getValue(),p.getKey())).collect(Collectors.toList());
+    }
+
+    public APDisease getDisease(String key) {
+        if (mondoDiseases.containsKey(key))
+            return mondoDiseases.get(key);
+        else
+            return null;
+    }
+
+
+    public String getMondoLabelWithId(String id){
+        if(mondoDiseases.containsKey(id)){
+            return mondoDiseases.get(id).getLabel();
+        }
+        else return "";
+    }
+
+    public String getMondoDefinitionWithId(String id){
+        if(mondoDiseases.containsKey(id)){
+            return mondoDiseases.get(id).getDefinition();
+        }
+        else return "";
+    }
+
+
+    /***
+     * Index is generated and given with artifact
+     * @param fileName
+     */
+    private void readFile(String fileName) {
+
+        slf4jLogger.info(String.format("Reading index file. Filename:%s", fileName));
+        String path =  "lexicons/"+ fileName;
+        InputStream input = getClass().getResourceAsStream("resources/" + path);
+        if (input == null) {
+            // this is how we load file within editor (eg eclipse)
+            input = DiseaseHandler.class.getClassLoader().getResourceAsStream(path);
+        }
+
+        try {
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input, "UTF-8"));
+            String line;
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            JSONObject jsonObject = (JSONObject) JSONValue.parse(stringBuilder.toString());
+            VERSION = jsonObject.get("version").toString();
+            STANDARD = jsonObject.get("standard").toString();
+            if(jsonObject.containsKey("diseases")){
+                JSONArray jsonDiseases = (JSONArray) jsonObject.get("diseases");
+
+                for(Object obj: jsonDiseases){
+                    JSONObject jsonDisease = (JSONObject) obj;
+
+                    ObjectMapper mapper = new ObjectMapper();
+                    APDisease apDisease =  mapper.readValue(jsonDisease.toString(), APDisease.class);
+                    mondoDiseases.put(apDisease.getMondoID(), apDisease);
+                    diseaseLabelToMondo.put(apDisease.getLabel(), apDisease.getMondoID());
+                    apDisease.getSynonyms().stream().forEach(s-> diseaseLabelToMondo.put(s, apDisease.getMondoID()));
+
+                }
+            }
+
+
+
+            reader.close();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+
+}
---
+++ src/main/java/au/org/garvan/kccg/annotations/pipeline/engine/entities/lexical/Drug/APDrug.java
@@ -0,0 +1,130 @@
+package au.org.garvan.kccg.annotations.pipeline.engine.entities.lexical.Drug;
+
+import au.org.garvan.kccg.annotations.pipeline.engine.entities.lexical.LexicalEntity;
+import au.org.garvan.kccg.annotations.pipeline.engine.enums.AnnotationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.json.simple.JSONObject;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by ahmed on 4/10/17.
+ */
+
+@AllArgsConstructor
+@Data
+public class APDrug extends LexicalEntity {
+
+    private String dbankUrl;
+    private String dbankID;
+    private String definition;
+    private String label;
+    private List<String> synonyms;
+    private boolean deprecated;
+
+    public APDrug(LinkedHashMap linkedHashMap) {
+        synonyms = new ArrayList<>();
+        if (linkedHashMap.containsKey("id") && linkedHashMap.get("type").equals("CLASS")) {
+            oboURI = linkedHashMap.get("id").toString();
+            mondoID = getMondoID(oboURI);
+
+            if (linkedHashMap.containsKey("meta")) {
+
+                LinkedHashMap linkedMeta = (LinkedHashMap) linkedHashMap.get("meta");
+                if (linkedMeta.containsKey("deprecated") && linkedMeta.get("deprecated").toString().equals("true"))
+                    deprecated = true;
+                if (linkedMeta.containsKey("definition")) {
+                    definition = ((LinkedHashMap) linkedMeta.get("definition")).get("val").toString();
+                }
+
+                if (linkedMeta.containsKey("synonyms")) {
+
+                    ArrayList lstSynonyms = (ArrayList) linkedMeta.get("synonyms");
+                    for (Object obj : lstSynonyms) {
+                        LinkedHashMap synonymMap =   ((LinkedHashMap)obj);
+                        if(synonymMap.get("pred").toString().equals("hasExactSynonym"))
+                            synonyms.add(synonymMap.get("val").toString());
+
+                    }
+                }
+            }
+
+            if (linkedHashMap.containsKey("lbl")) {
+                label = linkedHashMap.get("lbl").toString();
+            }
+
+
+        }
+        else
+            deprecated = true;
+
+
+
+    }
+
+
+
+
+    public JSONObject getAnnotationJsonForAnnotator(){
+        JSONObject jsonDiseases = new JSONObject();
+
+        jsonDiseases.put("oboURI", oboURI);
+        jsonDiseases.put("mondoID", mondoID);
+        jsonDiseases.put("definition", definition);
+        jsonDiseases.put("label", label);
+        jsonDiseases.put("synonyms", synonyms);
+        jsonDiseases.put("deprecated", deprecated);
+
+        return jsonDiseases;
+
+    }
+
+
+    public JSONObject getAnnotationJsonForAffinity(){
+        JSONObject jsonDiseases = new JSONObject();
+
+        jsonDiseases.put("queryId", this.getMondoID());
+        jsonDiseases.put("queryType", AnnotationType.DISEASE.toString());
+
+        String queryString = String.format("\"%s\"", this.label.trim());
+        if(synonyms.size()>0)
+        {
+            queryString = queryString + " OR " + synonyms.stream().map(s-> String.format("\"%s\"", s.trim())).collect(Collectors.joining(" OR "));
+
+        }
+        jsonDiseases.put("queryString", queryString);
+        return jsonDiseases;
+
+    }
+
+    String getMondoID(String uri) {
+        try {
+            int index = uri.lastIndexOf('/');
+            String id_part = uri.substring(index + 1, uri.length());
+            String[] parts = id_part.split("_");
+            return parts[0] + ":" + parts[1];
+        }
+        catch(Exception e){
+            System.out.println("Invalid URI");
+        }
+
+        return "";
+    }
+
+    public List<String> stringList() {
+        List<String> lstData = new ArrayList<>();
+
+        lstData.add(String.format("========%s========", mondoID ));
+        lstData.add(String.format("%s: %s", "Complete URI", oboURI));
+        lstData.add(String.format("%s: %s", "Preferred Label", label));
+        lstData.add(String.format("%s: %s", "Other Labels",  String.join("\n",synonyms)));
+
+        return lstData;
+    }
+
+
+}
